03e139bf30570780bdd1a2d0c1c1115286d9f6cb
RGAMEPC\ADIN <ryan.m.lam@gmail.com>
2023-10-05 16:28:32 -0700

    Part UI

--- src/app/task/menu-part/menu-part.component.ts
+++ src/app/task/menu-part/menu-part.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-part',
   templateUrl: './menu-part.component.html',
   styleUrls: ['./menu-part.component.sass']
 })
-export class MenuPartComponent {
+export class MenuPartComponent implements OnInit {
+  onDisplay: boolean = true;
+
+  ngOnInit() {
+
+  }
+
+  switchDisplay() { 
+    this.onDisplay = !this.onDisplay;
+  }
 
 }
